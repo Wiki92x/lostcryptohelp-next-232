919a35bc89a2e6edd74e0d0308aa3641aa087ee4
Wiki92x <gemissnasassben@gmail.com>
2025-04-06 17:10:55 +0400

    Create main.jsx

---
+++ frontend/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+import LostCryptoHelp from './components/LostCryptoHelp.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <LostCryptoHelp />
+  </React.StrictMode>
+)
